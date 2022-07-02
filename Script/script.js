6b18894cd6db637b92d306ec12b3f3972bfdc484
LPandorf <leticiapandorf@gmail.com>
2022-07-02 17:00:56 -0300

    fsd

--- Script/script.js
+++ Script/script.js
@@ -1,2 +1,18 @@
 
+//recarrega a pagina
+var btn = document.querySelector("#refresh");
+btn.addEventListener("click", function() {
+    
+    location.reload();
+});
 
+paraPage5() //vai da page 4 para a page 5
+
+paraPage6() //vai da page 5 para a page 6
+
+
+voltarParaHome(){ //volta para page 1 ou page 2
+
+}
+
+criarQuizz() //vai para page 4
