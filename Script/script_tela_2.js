9db5860b48443f31b315fd9e4a14cd5e3af9a809
Kauan De Mello <107003647+KauanDeMello@users.noreply.github.com>
2022-07-04 14:31:51 -0300

    js

--- Script/script_tela_2.js
+++ Script/script_tela_2.js
@@ -1,28 +1,36 @@
+const question = document.getElementById('question'); 
+const choices = Array.from(document.getElementsByClassName('perguntas_quizz'));
+console.log(choices);
 
-//recarrega a pagina
-/* var btn = document.querySelector("#refresh");
-btn.addEventListener("click", function() {
-    
-    location.reload();
-/ }); */
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let avaibleQuestions = {};
 
-
-
-
-
-
-
-
-
-
-
-//
-paraPage5() //vai da page 4 para a page 5
-
-paraPage6() //vai da page 5 para a page 6
-
-voltarParaHome() //volta para page 1 ou page 2
-
-criarQuizz() //vai para page 4
-//
+let questions = [
+    {
+    question:"Qual queijo é o brasileiro?",
+    choice1:"Queijo Cheddar",
+    choice2:"Queijo Canastra",
+    choice3:"Queijo Brie",
+    choice4:"Queijo Gorgonzola",
+    answer: 2
+    },
+    {
+    question:"Qual país produz queijo que tem larvas?",
+    choice1:"Itália",
+    choice2:"China",
+    choice3:"Austrália",
+    choice4:"Chile",
+    answer: 1
+    },{
+    question:"Qual estado brasileiro inventou o pão e queijo?",
+    choice1:"Minas Gerais",
+    choice2:"São Paulo",
+    choice3:"Santa Catarina",
+    choice4:"Pernambuco",
+    answer: 1
+    }
+]
 
